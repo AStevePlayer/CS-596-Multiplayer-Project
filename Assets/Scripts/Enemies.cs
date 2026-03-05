47929206a17bfaeede04cf21e15267ee91313cdd
AStevePlayer <fgomez7184@sdsu.edu>
2026-03-04 23:28:38 -0800

    Enemies face player now

--- Assets/Scripts/Enemies.cs
+++ Assets/Scripts/Enemies.cs
@@ -5,9 +5,10 @@ using System.Collections.Generic;
 public class Enemies : MonoBehaviour {
     public Player targetPlayer;
     public float targetAngle;
-    public float targetSpeed;
+    //public float targetSpeed;
     public float screenLimitX;
     public float screenLimitY;
+    public Vector3 endPoint;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
         Vector2 screenSize = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
@@ -16,12 +17,13 @@ public class Enemies : MonoBehaviour {
         targetPlayer = GameObject.FindObjectOfType(typeof(Player)) as Player;
         Vector2 offset = new Vector2(transform.position.x - targetPlayer.transform.position.x, transform.position.y - targetPlayer.transform.position.y);
         this.transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg);
-        targetSpeed = 0.1f;
+        endPoint = this.transform.position - (this.transform.right * 50.0f);
+        this.transform.eulerAngles += new Vector3(0, 0, 90);
     }
 
     // Update is called once per frame
     void FixedUpdate() {
-        this.transform.position += -transform.right * targetSpeed;
+        this.transform.position = Vector3.MoveTowards(this.transform.position, endPoint, 0.05f);
         if (Mathf.Abs(this.transform.position.x) > screenLimitX || Mathf.Abs(this.transform.position.y) > screenLimitY) {
             Destroy(gameObject);
         }
